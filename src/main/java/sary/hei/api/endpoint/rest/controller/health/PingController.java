1b84366ab89b58a575a0640e780251489cd9c6ab
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-02-15 15:38:16 +0300

    feat: poja sary

---
+++ src/main/java/sary/hei/api/endpoint/rest/controller/health/PingController.java
@@ -0,0 +1,28 @@
+package sary.hei.api.endpoint.rest.controller.health;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sary.hei.api.PojaGenerated;
+import sary.hei.api.repository.DummyRepository;
+import sary.hei.api.repository.DummyUuidRepository;
+
+@PojaGenerated
+@RestController
+@AllArgsConstructor
+public class PingController {
+
+  DummyRepository dummyRepository;
+  DummyUuidRepository dummyUuidRepository;
+
+  public static final ResponseEntity<String> OK = new ResponseEntity<>("OK", HttpStatus.OK);
+  public static final ResponseEntity<String> KO =
+      new ResponseEntity<>("KO", HttpStatus.INTERNAL_SERVER_ERROR);
+
+  @GetMapping("/ping")
+  public String ping() {
+    return "pong";
+  }
+}
